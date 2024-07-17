9994a23d720c93bc8f1ac92e191cb82d6e08f128
GabyCastt <gabykim928@gmail.com>
2024-07-17 16:18:50 -0500

    Estilos

---
+++ Ejercicio/views/html/menu.php
@@ -0,0 +1,36 @@
+<div class="sidebar text-light vh-100" style="width: 240px; background-color: #124559;">
+    <nav class="navbar flex-column align-items-start h-100">
+        <a href="../Dashboard/" class="navbar-brand p-4 w-100">
+            <h3 class="text-white fw-bold fs-4">
+                <i class="fas fa-shield-alt me-1"></i>DEFENSA FINAL
+            </h3>
+        </a>
+    
+        <ul class="nav flex-column w-100 mt-4">
+            <li class="nav-item w-100">
+                <a href="./amigos.php" class="nav-link text-light py-3 px-4 w-100 d-flex align-items-center">
+                    <i class="fas fa-user-friends me-3 fs-5"></i>
+                    <span>Amigos</span>
+                </a>
+            </li>
+            <li class="nav-item w-100">
+                <a href="./grupos.php" class="nav-link text-light py-3 px-4 w-100 d-flex align-items-center">
+                    <i class="fas fa-users me-3 fs-5"></i>
+                    <span>Grupos</span>
+                </a>
+            </li>
+            <li class="nav-item w-100">
+                <a href="./grupos.php" class="nav-link text-light py-3 px-4 w-100 d-flex align-items-center">
+                    <i class="fas fa-users me-3 fs-5"></i>
+                    <span>Grupos</span>
+                </a>
+            </li>
+            <li class="nav-item w-100">
+                <a href="./grupos.php" class="nav-link text-light py-3 px-4 w-100 d-flex align-items-center">
+                    <i class="fas fa-users me-3 fs-5"></i>
+                    <span>Grupos</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</div>
